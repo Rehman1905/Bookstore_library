bf1a2aa77670c2068a3aff2f0a69b0c07c2ff9bb
Rehman1905 <ll.mmdoff.ll@gmail.com>
2024-03-27 19:22:56 +0400

    OK

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -344,61 +344,33 @@ function updateCommit(e) {
     for (let i in data) {
 
         var newCommentDiv = document.createElement('div');
-        newCommentDiv.classList.add('commentText2');
-
-        var newTopDiv = document.createElement('div');
-        newTopDiv.classList.add('textTop');
-
-
-        var newUserNameDiv = document.createElement('div');
-        newUserNameDiv.classList.add('userName');
-        newUserNameDiv.textContent = 'Anonim';
-
-        var newDateDiv = document.createElement('div');
-        newDateDiv.classList.add('dateUser');
-        var now = new Date();
-        var time = now.getHours() + ':' + now.getMinutes();
-        newDateDiv.textContent = data[i].date;
-
-        newTopDiv.appendChild(newUserNameDiv);
-        newTopDiv.appendChild(newDateDiv);
-
-        var newTextDiv = document.createElement('div');
-        newTextDiv.classList.add('textArea');
-        newTextDiv.textContent = data[i].text;
-
-        newCommentDiv.appendChild(newTopDiv);
-        newCommentDiv.appendChild(newTextDiv);
-
-        commentText.appendChild(newCommentDiv);
-        newCommentDiv.classList.add('text2');
-
-=======
-
-        var newUserNameDiv = document.createElement('div');
-        newUserNameDiv.classList.add('userName');
-        newUserNameDiv.textContent = 'Anonim';
-
-        var newDateDiv = document.createElement('div');
-        newDateDiv.classList.add('dateUser');
-        var now = new Date();
-        var time = now.getHours() + ':' + now.getMinutes();
-        newDateDiv.textContent = data[i].date;
-
-        newTopDiv.appendChild(newUserNameDiv);
-        newTopDiv.appendChild(newDateDiv);
-
-        var newTextDiv = document.createElement('div');
-        newTextDiv.classList.add('textArea');
-        newTextDiv.textContent = data[i].text;
-
-        newCommentDiv.appendChild(newTopDiv);
-        newCommentDiv.appendChild(newTextDiv);
-
-        commentText.appendChild(newCommentDiv);
-        newCommentDiv.classList.add('text2');
-
-
+            newCommentDiv.classList.add('commentText2');
+  
+            var newTopDiv = document.createElement('div');
+            newTopDiv.classList.add('textTop');
+  
+            var newUserNameDiv = document.createElement('div');
+            newUserNameDiv.classList.add('userName');
+            newUserNameDiv.textContent = 'Anonim';
+  
+            var newDateDiv = document.createElement('div');
+            newDateDiv.classList.add('dateUser');
+            var now = new Date();
+            var time = now.getHours() + ':' + now.getMinutes();
+            newDateDiv.textContent = data[i].date;
+  
+            newTopDiv.appendChild(newUserNameDiv);
+            newTopDiv.appendChild(newDateDiv);
+  
+            var newTextDiv = document.createElement('div');
+            newTextDiv.classList.add('textArea');
+            newTextDiv.textContent = data[i].text;
+  
+            newCommentDiv.appendChild(newTopDiv);
+            newCommentDiv.appendChild(newTextDiv);
+  
+            commentText.appendChild(newCommentDiv);
+            newCommentDiv.classList.add('text2');
 
     }
 }
@@ -407,4 +379,4 @@ var backButton = document.querySelector('.backButton')
 backButton.addEventListener('click', function (e) {
     e.preventDefault
     window.location.href = './catalog.html'
-
+})
