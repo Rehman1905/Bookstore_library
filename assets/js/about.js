efd2b6f7e1cf39c5ed4d51213602940e3b3c273c
Husuagayev1997@gmail.com <husuagayev1997@gmail.com>
2024-03-27 17:55:50 +0400

    commit3

--- assets/js/about.js
+++ assets/js/about.js
@@ -89,6 +89,17 @@ document.querySelector('#btnJoin').addEventListener('click', async function (e) 
     })
     nameJoin.value = ''
     emailJoin.value = ''
+    const section4 = document.querySelector('.section4')
+section4.classList.add('rightAnimation')
+    section4.style.display = 'flex'
+    setTimeout(function () {
+        section4.classList.remove('rightAnimation')
+        section4.classList.add('leftAnimation')
+    }, 1000)
+    setTimeout(function () {
+        section4.classList.remove('leftAnimation')
+        section4.style.display = 'none'
+    }, 1500)
 })
 document.querySelector('#exit').addEventListener('click', function (e) {
     section5.style.display = 'none'
